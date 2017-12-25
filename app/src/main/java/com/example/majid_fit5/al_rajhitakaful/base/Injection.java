7dcefbd082d825c4c560fcacdbb08a995f008ddd
Majid_Fit5 <majid2_fit5@hotmail.com>
2017-12-25 15:15:41 +0300

    Adding Base Folder

---
+++ app/src/main/java/com/example/majid_fit5/al_rajhitakaful/base/Injection.java
@@ -0,0 +1,14 @@
+package com.example.majid_fit5.al_rajhitakaful.base;
+
+import com.example.majid_fit5.al_rajhitakaful.data.DataRepository;
+import com.example.majid_fit5.al_rajhitakaful.data.RemoteDataSource;
+
+/**
+ * Created by Eng. Abdulmajid Alyafey on 12/25/2017.
+ */
+
+public class Injection {
+    public static DataRepository provideDataRepository() {
+        return DataRepository.getInstance(RemoteDataSource.getInstance());
+    }
+}

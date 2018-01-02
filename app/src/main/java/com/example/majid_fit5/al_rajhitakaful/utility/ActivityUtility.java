b6cfc19b5c733a40a9141fd3456f945382b5364a
BASH <m.ahmed@morniksa.com>
2018-01-02 11:00:26 +0300

    DEV_2 after login deleted

--- app/src/main/java/com/example/majid_fit5/al_rajhitakaful/utility/ActivityUtility.java
+++ app/src/main/java/com/example/majid_fit5/al_rajhitakaful/utility/ActivityUtility.java
@@ -8,7 +8,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.app.Fragment;
 
-import com.example.majid_fit5.al_rajhitakaful.login.mobilephoneinsertion.MobilePhoneInsertionFragment;
+
 
 /**
  * Created by Eng. Abdulmajid Alyafey on 12/25/2017.

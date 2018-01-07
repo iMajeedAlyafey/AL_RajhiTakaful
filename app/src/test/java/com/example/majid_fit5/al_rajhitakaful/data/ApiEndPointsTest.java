a8e2821aeb84f7a72d16e84da0273a7c03e5ce26
Majid_Fit5 <majid2_fit5@hotmail.com>
2018-01-07 16:11:47 +0300

    Stable Version. Fix Design of some activities.

---
+++ app/src/test/java/com/example/majid_fit5/al_rajhitakaful/data/ApiEndPointsTest.java
@@ -0,0 +1,44 @@
+package com.example.majid_fit5.al_rajhitakaful.data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Eng. Abdulmajid Alyafey on 1/7/2018.
+ */
+public class ApiEndPointsTest {
+    @Test
+    public void otp() throws Exception {
+
+    }
+
+    @Test
+    public void login() throws Exception {
+    }
+
+    @Test
+    public void logout() throws Exception {
+    }
+
+    @Test
+    public void createOrder() throws Exception {
+    }
+
+    @Test
+    public void getCurrentUser() throws Exception {
+    }
+
+    @Test
+    public void cancelOrder() throws Exception {
+    }
+
+    @Test
+    public void getOrder() throws Exception {
+    }
+
+    @Test
+    public void uploadPhoto() throws Exception {
+    }
+
+}
